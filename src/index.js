9929ae7f4048f8bfb81baee374c3d015cfba435b
Jjjulietta <jjjuliettai@gmail.com>
2023-01-04 17:36:18 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+  let str = String(Math.abs(n));
+  let strTwo = '';
   
-}
+    for(let i = str.length - 1; i >= 0; i --){
+    strTwo += str[i];
+    }
+    return +strTwo;
+  }
+
+
+
+
